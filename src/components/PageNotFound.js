13db83be2fc7bbf3ef45d1ad58697ffaeb1710e0
LeoU-98 <149756565+LeoU-98@users.noreply.github.com>
2023-11-03 11:26:37 +0200

    Update PageNotFound.js

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -9,7 +9,7 @@ export default function NavBar() {
         <div className="page-not-found">
             <div>
                 <h1 className="page-not-found-heading">Sorry,</h1>
-                <img src="./Images/NotFound.png" alt="" className="page-not-found-img"/>
+                <img src=".Images/NotFound.png" alt="" className="page-not-found-img"/>
             </div>
             <p className="page-not-found-paragraph">404 - Page Not Found</p>
 
