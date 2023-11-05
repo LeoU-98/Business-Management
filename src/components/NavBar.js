91d1842636654e36e04b5cb290d1fb20ac47e1bf
LeoU-98 <149756565+LeoU-98@users.noreply.github.com>
2023-11-05 17:23:50 +0200

    fix home page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,11 +18,11 @@ export default function NavBar() {
                 <div></div>
             </div>
             <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/About">About</NavLink></li>
-                <li><NavLink to="/Services">Services</NavLink></li>
-                <li><NavLink to="/Contact">Contact</NavLink></li>
-                <li><NavLink to="/SignUp">Sign Up</NavLink></li>
+                <li><NavLink to="/" exact   >Home</NavLink></li>
+                <li><NavLink to="/About"    >About</NavLink></li>
+                <li><NavLink to="/Services" >Services</NavLink></li>
+                <li><NavLink to="/Contact"  >Contact</NavLink></li>
+                <li><NavLink to="/SignUp"   >Sign Up</NavLink></li>
             </ul>
         </nav>
     </div>
